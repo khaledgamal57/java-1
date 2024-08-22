0700a38483e4b75a4300bae7dd574aef8ad11352
khaledgamal57 <khalidelgohary63@gmail.com>
2024-08-22 19:01:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const myButton = document.getElementById("my button");
+const my Img = document.getElementById("my img");
+
+my myButton.addEventListener( "click" ,event => {
+ if (myImg.visbility ==="hidden"){
+    myImg.style,visbility ="visible";
+    myButton.textContent ="hide";
+
+ }
+else{
+myImg.visbility ="none";
+myButton.textContent ="show";
+}
+
+
+});
